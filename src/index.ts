b9f162b49fa687c26f9b9f58773d9110caa07e7d
Pascal ABEDI <abedipascal2@gmail.com>
2025-07-01 14:39:03 +0100

    creating api page src/index.ts...

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello !")
+import express from "express";
+
+require("dotenv").config()
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+const PORT = process.env.PORT || 8000
+
+app.use(express.json())
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
