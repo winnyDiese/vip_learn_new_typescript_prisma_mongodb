9946d7ec90e0063d320492c6b79d51c380e2ca9e
Pascal ABEDI <abedipascal2@gmail.com>
2025-07-01 14:57:32 +0100

    mdfgbknmoin

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 require("dotenv").config()
 const cors = require('cors')
@@ -12,4 +12,18 @@ app.use(express.json())
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+
+// create an api
+app.get("/customers", async (req:Request, res:Response) => {
+    const customers = [
+        { name: "John Doe", email: "john.doe@gmail.com", phone: "+123456789" },
+        { name: "Alice Smith", email: "alice.smith@yahoo.com", phone: "+987654321" },
+        { name: "Carlos Mendes", email: "carlos.mendes@hotmail.com", phone: "+3344556677" },
+        { name: "Fatima Zahra", email: "fatima.zahra@outlook.com", phone: "+1122334455" },
+        { name: "Kenji Takahashi", email: "kenji.takahashi@gmail.com", phone: "+5566778899" },
+    ];
+
+    return res.status(200).json(customers)
 })
