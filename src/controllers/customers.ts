81671673023d94c3bebd820acb3e42877ab28f49
Pascal ABEDI <abedipascal2@gmail.com>
2025-07-01 18:08:26 +0100

    skdfjhvb

---
+++ src/controllers/customers.ts
@@ -0,0 +1,35 @@
+import { bd } from "@/db/db"
+import { Request, Response} from "express"
+
+export async function createCustomer(req: Request, res: Response) {
+    const {name, email, phone} = req.body
+
+    try {
+        const newCustomer = await db.customer.create({
+            data: {
+                name,
+                email,
+                phone
+            }
+        })
+
+        return res.status(201).json(newCustomer)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function getCustomers(req: Request, res: Response){
+    try {
+        const customers = await db.customerfindMany({
+            orderBy: {
+                createdAt: "desc"
+            }
+        })
+
+        return res.status(200).json(customers)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
